540fab1f3bf3ba6104ca5b0053523e8dca359bc7
once-ler <htao@eztier.com>
2016-12-23 19:39:07 -0500

    Add Observer class

--- src/observer.js
+++ src/observer.js
@@ -1,1 +1,31 @@
 /* @flow */
+
+import {Observable, Subscription, Scheduler} from 'rxjs';
+import type {rxweb$SocketType, rxweb$FilterFunc} from './rxweb';
+import type { rxweb$Task } from './rxweb';
+
+export class rxweb$Observer<rxweb$SocketType> {
+  _observer: Observable<rxweb$Task<rxweb$SocketType>>;
+
+  constructor(o: Observable<rxweb$Task<rxweb$SocketType>>, filterFunc: rxweb$FilterFunc<rxweb$SocketType>) {
+    this._observer = o
+      .observeOn(Scheduler.queue)
+      .filter(filterFunc);
+  }
+
+  observable(): Observable<rxweb$Task<rxweb$SocketType>> {
+    return this._observer;
+  }
+
+  subscribe(
+    onNext: ?(value: rxweb$Task<rxweb$SocketType>) => mixed,
+    onError: ?(error: any) => mixed,
+    onCompleted: ?() => mixed
+  ) {
+    this._observer.subscribe(onNext, onError, onCompleted);
+  }
+}
+
+declare module "rxweb" {
+  declare var rxweb$Observer: rxweb$Observer<rxweb$SocketType>; 
+}
--- src/subject.js
+++ src/subject.js
@@ -1,1 +1,4 @@
 /* @flow */
+
+import type {rxweb$SocketType } from './rxweb';
+import { rxweb$Observer } from './observer';
