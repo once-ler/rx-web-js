efb9c69f765b0333c3882fb1901948af1400e3aa
once-ler <htao@eztier.com>
2016-12-31 19:40:10 -0500

    Add should

--- src/__tests__/http-test.js
+++ src/__tests__/http-test.js
@@ -12,4 +12,58 @@
 /* eslint-disable max-len */
 import request from 'supertest-as-promised';
 import { describe, it } from 'mocha';
+import should from 'should';
 
+import type {
+  rxweb$FilterFunc,
+  rxweb$SubscribeFunc,
+  Redux$Middleware,
+  Redux$Store,
+  Redux$State,
+  Redux$Action
+} from '../rxweb';
+
+import {
+  rxweb$Task,
+  rxweb$Middleware,
+  rxweb$Subject,
+  rxweb$Server,
+  rxweb$Route
+} from '../rxweb';
+
+function promiseTo(fn) {
+  return new Promise((resolve, reject) => {
+    fn((error, result) => error ? reject(error) : resolve(result));
+  });
+}
+
+describe('test harness', () => {
+
+  it('resolves callback promises', async () => {
+    const resolveValue = {};
+    const result = await promiseTo(cb => cb(null, resolveValue));
+    expect(result).to.equal(resolveValue);
+  });
+
+  it('rejects callback promises with errors', async () => {
+    const rejectError = new Error();
+    let caught;
+    try {
+      await promiseTo(cb => cb(rejectError));
+    } catch (error) {
+      caught = error;
+    }
+    expect(caught).to.equal(rejectError);
+  });
+
+});
+
+decribe('can create koa server', () => {
+
+  it('resolves to an instance of rxweb$Server', () =>{
+    const server = new rxweb$Server(3000);
+
+    server.should.be.an.instanceOf(Object);
+  });
+
+});
